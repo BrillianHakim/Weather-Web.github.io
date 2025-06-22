1e39250a7cb837370947ab5674b720076ae38f96
BrillianHakim <bintangbrillianhakim@gmail.com>
2025-06-22 22:23:20 +0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Weather-Web.github.io/',
+  // base: '/Weather-Web.github.io/',
 })
